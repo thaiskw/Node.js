181a8a68f6367a91bb8fc8f7aee79fcd2db46619
Thaina <thaina.lima47@etec.sp.gov.br>
2025-09-16 17:30:41 -0300

    bd implementado

--- CRUD/config/firebase.js
+++ CRUD/config/firebase.js
@@ -1,8 +1,7 @@
-// Importa apenas o que é necessário para Node.js
+// config/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
-// Config do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCappDU27vgFjRxIlu2fMejQOmQroITUyY",
   authDomain: "crud-node-147f5.firebaseapp.com",
@@ -11,12 +10,9 @@ const firebaseConfig = {
   storageBucket: "crud-node-147f5.firebasestorage.app",
   messagingSenderId: "18563621591",
   appId: "1:18563621591:web:88d33fc1ea86f2b44a0692"
-  // measurementId não precisa no Node.js
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getDatabase(app);
 
-// Exporta db
 export { db };
